2796ee23a6edb8a1e0c2f1224bb1b4a12b06472d
Danilo Obradović <daki.0037@gmail.com>
2024-12-15 20:17:29 +0100

    Initial file strucure

---
+++ backend/xssposure/src/main/java/com/vulnerable/xssposure/XsSposureApplication.java
@@ -0,0 +1,13 @@
+package com.vulnerable.xssposure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsSposureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsSposureApplication.class, args);
+	}
+
+}
---
+++ backend/xssposure/src/test/java/com/vulnerable/xssposure/XsSposureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vulnerable.xssposure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsSposureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
